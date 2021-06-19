43f571a62978d12e77a9d6883b9285422f888baf
Oleksandr Vasilkov <vasilkoff1337@gmail.com>
2021-06-19 17:57:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.c1b59cf86946657189d5d38559a54b0c.js"
 );
 
 self.addEventListener('message', (event) => {
